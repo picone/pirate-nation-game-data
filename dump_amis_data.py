308c45a9ebdf21c4c1ef37f22a9bc3beec21764f
cj.he <cj.he@aftership.com>
2023-11-01 00:44:20 +0800

    add quest data

--- dump_amis_data.py
+++ dump_amis_data.py
@@ -62,8 +62,72 @@ def format_loot_tables(loot_tables, items):
     return list(map(format_loot_item, loot_tables))
 
 
-def dump_quests(quests):
-    print(quests)
+def format_quests(quests, loot_tables, items, quest_assets):
+    item_map = dict(map(lambda x: (x['tokenId'], x), items))
+    loot_map = dict(map(lambda x: (x['id'], x), loot_tables))
+
+    def format_inputs(inp):
+        if inp['tokenPointer']['tokenType'] == 'ERC20' and inp['tokenPointer']['tokenContract']['id'] == PGLD_CONTACT_ADDRESS:
+            return {
+                'lootType': 'ERC20',
+                'name': 'PGLD',
+                'image': 'https://piratenation.game/game_assets/currency/gold.png',
+                'amount': int(int(inp['tokenPointer']['amount']) / 1e18),
+            }
+        elif inp['tokenPointer']['tokenType'] == 'ERC721' and inp['tokenPointer']['tokenContract']['id'] == PIRATE_CONTRACT_ADDRESS:
+            pass  # TODO
+        elif inp['tokenPointer']['tokenType'] == 'ERC1155' and inp['tokenPointer']['tokenContract']['id'] == ITEM_CONTRACT_ADDRESS:
+            item = item_map[int(inp['tokenPointer']['tokenId'])]
+            return {
+                'lootType': 'ERC1155',
+                'name': item['name'],
+                'image': None if 'image_trait' not in item else item['image_trait'],
+                'amount': int(inp['tokenPointer']['amount']),
+            }
+        else:
+            raise Exception('Unknown input: ' + inp)
+
+    def format_outputs(out):
+        if out['lootType'] == 'ERC20' and out['tokenContract']['id'] == PGLD_CONTACT_ADDRESS:
+            return {
+                'lootType': 'ERC20',
+                'name': 'PGLD',
+                'image': 'https://piratenation.game/game_assets/currency/gold.png',
+                'amount': int(int(out['amount']) / 1e18),
+            }
+        elif out['lootType'] == 'ERC721' and out['tokenContract']['id'] == PIRATE_CONTRACT_ADDRESS:
+            pass  # TODO
+        elif out['lootType'] == 'ERC1155' and out['tokenContract']['id'] == ITEM_CONTRACT_ADDRESS:
+            item = item_map[int(out['lootId'])]
+            return {
+                'lootType': 'ERC1155',
+                'name': item['name'],
+                'image': None if 'image_trait' not in item else item['image_trait'],
+                'amount': int(out['amount']),
+            }
+        elif out['lootType'] == 'LOOT_TABLE':
+            loot_item = loot_map[int(out['lootId'])]
+            return {
+                'lootType': 'LOOT_TABLE',
+                'name': 'loot_' + out['lootId'],
+                'items': loot_item['group'],
+                'amount': int(out['amount']),
+            }
+        else:
+            raise Exception('Unknown output: ' + out)
+
+    def format_quest(quest):
+        return {
+            'id': int(quest['id']),
+            'name': '' if quest['id'] not in quest_assets else quest_assets[quest['id']]['name'],
+            'success_xp': int(quest['successXp']),
+            'cooldown_seconds': int(quest['cooldownSeconds']),
+            'total_completed': int(quest['totalCompleted']),
+            'inputs': list(map(format_inputs, filter(lambda x: x['consumable'], quest['inputs']))),
+            'outputs': list(map(format_outputs, quest['loots'])),
+        }
+
+    return list(map(format_quest, quests))
 
 
 def write_list_to_json_file(filename: str, data: [dict]):
@@ -90,7 +154,9 @@ def main():
     write_list_to_json_file('loot_tables.json', loot_tables)
 
     quests = loader.get_quests()
-    dump_quests(quests)
+    quest_assets = loader.get_quest_assets()
+    quests = format_quests(quests, loot_tables, items, quest_assets)
+    write_list_to_json_file('quests.json', quests)
 
 
 if __name__ == '__main__':
